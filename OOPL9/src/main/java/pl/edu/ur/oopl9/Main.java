9cf3d16d53c440769062dd989824b75370bc4886
Kamil Machański <>
2018-05-14 20:25:29 +0200

    zadanie 4

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -7,17 +7,14 @@ package pl.edu.ur.oopl9;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 
-
-/**
- *
- * @author maarchyl
- */
 public class Main {
 
     /**
@@ -25,11 +22,12 @@ public class Main {
      */
     public static void main(String[] args) {
         // 1
-        
+
         Random rand = new Random();
         Scanner input = new Scanner(System.in);
-        
+
         int[] tab = new int[100];
+
         /*
         for(int i=0; i<tab.length;i++)
         {
@@ -42,8 +40,7 @@ public class Main {
             System.out.println(tab[i]);
             
         }
-        */
-        
+         */
         // 2
         /*
         for(int i=0; i<tab.length;i++)
@@ -65,10 +62,9 @@ public class Main {
 		System.out.print(iterator.next() + " ");
 	}
 	System.out.println();
-	*/
-        
+         */
         // 3
-        
+        /*
         ArrayList<Samochod> samochody = new ArrayList<Samochod>();
         
         
@@ -87,14 +83,75 @@ public class Main {
             samochody.add(new Samochod(marka, nazwa, cena));
             System.out.println();
         }
+        System.out.println(samochody);
+        /*
         for(int i=0;i<samochody.size();i++)
         {
             System.out.println("Auto "+i+" :"+samochody.get(i));
         }
+         */
         
         
-	
+        // 4
         
+        Map<String, String> slowa = new HashMap<>(20);
+        slowa.put("pies", "dog");
+        slowa.put("kot", "cat");
+        slowa.put("czapka", "cap");
+        slowa.put("mysz", "mouse");
+        slowa.put("dom", "house");
+        slowa.put("kwiat", "flower");
+        slowa.put("komputer", "computer");
+        slowa.put("telewizja", "TV");
+        slowa.put("krzeslo", "chair");
+        slowa.put("powietrze", "air");
+        slowa.put("wszystko", "all");
+        slowa.put("zawsze", "always");
+        slowa.put("wiek", "age");
+        slowa.put("rozmiar", "size");
+        slowa.put("niebo", "sky");
+        slowa.put("spac", "sleep");
+        slowa.put("siostra", "sit");
+        slowa.put("spiewac", "sing");
+        slowa.put("snieg", "snow");
+        slowa.put("syn", "son");
+
+        int k = 1;
+
+        while (k == 1) {
+
+            System.out.println("Wpisz słowo które chcesz przetłumaczyć na angielski: "
+                    + "(wpisz 'koniec' jesli chcesz zakończyć) ");
+
+            String n = input.next();
+
+            if ("pies".equals(n))  System.out.println(slowa.get("pies"));   
+             else if ("kot".equals(n))  System.out.println(slowa.get("kot"));   
+             else if ("czapka".equals(n))  System.out.println(slowa.get("czapka"));   
+             else if ("mysz".equals(n))  System.out.println(slowa.get("mysz"));   
+             else if ("dom".equals(n))  System.out.println(slowa.get("dom"));   
+             else if ("kwiat".equals(n))  System.out.println(slowa.get("kwiat"));   
+             else if ("komputer".equals(n))  System.out.println(slowa.get("komputer"));  
+             else if ("telewizja".equals(n))  System.out.println(slowa.get("telewizja"));   
+             else if ("krzeslo".equals(n))  System.out.println(slowa.get("krzeslo"));   
+             else if ("powietrze".equals(n))  System.out.println(slowa.get("powietrze"));  
+             else if ("wszystko".equals(n))  System.out.println(slowa.get("wszystko"));   
+             else if ("zawsze".equals(n))  System.out.println(slowa.get("zawsze"));  
+             else if ("wiek".equals(n))  System.out.println(slowa.get("wiek"));   
+             else if ("rozmiar".equals(n))  System.out.println(slowa.get("rozmiar"));   
+             else if ("niebo".equals(n))  System.out.println(slowa.get("niebo"));  
+             else if ("spac".equals(n))  System.out.println(slowa.get("spac"));  
+             else if ("siostra".equals(n))  System.out.println(slowa.get("siostra"));  
+             else if ("spiewac".equals(n))  System.out.println(slowa.get("spiewac"));   
+             else if ("snieg".equals(n))  System.out.println(slowa.get("snieg"));   
+             else if ("syn".equals(n))  System.out.println(slowa.get("syn"));   
+             else if ("koniec".equals(n))  k = 0;  
+             else  System.out.println("Nie ma takiego słowa w bazie");
+                
+            
+        }
+
+       
     }
-    
+
 }
